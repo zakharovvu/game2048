9c17e1416bae79bf3069abe19e31786555dedd60
Slava <zakharovvu@gmail.com>
2019-05-06 20:06:55 +0300

    Updates

---
+++ precache-manifest.d9502d2cd61c81dc7e063c8007fc635e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e9eb43de44b849a046416e536dd8dc1",
+    "url": "/game2048/index.html"
+  },
+  {
+    "revision": "9a86431811409c7a9953",
+    "url": "/game2048/static/css/main.baaa07f1.chunk.css"
+  },
+  {
+    "revision": "1744db918d7799a6e6d6",
+    "url": "/game2048/static/js/2.164617b5.chunk.js"
+  },
+  {
+    "revision": "9a86431811409c7a9953",
+    "url": "/game2048/static/js/main.42e50684.chunk.js"
+  },
+  {
+    "revision": "ac10bad9cc8031b8938a",
+    "url": "/game2048/static/js/runtime~main.c0130fb5.js"
+  }
+]);
